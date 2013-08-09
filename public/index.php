0ecacec677b2e890593d024aac8ddccfab817c11
Fabricio Nogueira <fabricionogueira@CERCOMP-608830.(none)>
2013-08-09 15:28:12 -0300

    Organização do projeto

--- public/index.php
+++ public/index.php
@@ -1,26 +1,13 @@
 <?php
 
-// Define path to application directory
-defined('APPLICATION_PATH')
-    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
+call_user_func(function() {
 
-// Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+    require_once '../library/autoload.php';
 
-// Ensure library/ is on include_path
-set_include_path(implode(PATH_SEPARATOR, array(
-    realpath(APPLICATION_PATH . '/../library'),
-    get_include_path(),
-)));
-
-/** Zend_Application */
-require_once 'Zend/Application.php';
-
-// Create application, bootstrap, and run
-$application = new Zend_Application(
-    APPLICATION_ENV,
-    APPLICATION_PATH . '/configs/application.ini'
-);
-$application->bootstrap()
+    // Create application, bootstrap, and run
+    $application = new Zend_Application(
+            APPLICATION_ENV, APPLICATION_PATH . '/configs/application.ini'
+    );
+    $application->bootstrap()
             ->run();
+});
